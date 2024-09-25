5397c3bf9bef4cf53381444fc8175c9060bdadac
Chico Han <minhanacc@gmail.com>
2024-09-25 11:05:05 +0630

    change java version 11 to 17 and update yml

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RoboList
+
+# Using Local database with room
+# Password protection with AES encryption
+# Domain layer use-cases
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = org.gradle.api.JavaVersion.VERSION_11
-        targetCompatibility = org.gradle.api.JavaVersion.VERSION_11
+        sourceCompatibility = org.gradle.api.JavaVersion.VERSION_17
+        targetCompatibility = org.gradle.api.JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
